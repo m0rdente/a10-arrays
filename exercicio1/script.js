17a1585fd38cae4733b9ee5ad7265523f7da067e
Priscila Mordente <priscilamordente@gmail.com>
2022-07-28 20:40:01 -0300

    Exercicio a010 corrigido

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,8 @@
 const numeros = [1,10,15,20,25]
 const nomes = ["Kagome", "Inuyasha","Kikyou"]
-const booleanos = [true, false]
+const misto = ["Game of Thrones", 4, false]
 
 console.log (numeros)
 console.log (nomes)
-console.log (booleanos)
+console.log (misto)
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,14 +1,18 @@
 const numeros = [1,10,15,20,25]
 const nomes = ["Kagome", "Inuyasha","Kikyou"]
-const booleanos = [true, false]
+const misto = ["Game of Thrones", 4, false]
 
 const numerosCopia = [1,10,15,20,25]
 numerosCopia.push(30)
 const nomesCopia = ["Kagome", "Inuyasha","Kikyou"]
 nomesCopia.pop("Kikyou")
-const booleanosCopia = [true, false]
-booleanosCopia.splice(1,1)
+const mistoCopia = ["Game of Thrones", 4, false]
+mistoCopia.splice(2,1)
 
-console.log("Primeira Array original", numeros, "\nPrimeira Array copia", numerosCopia)
-console.log("Segunda Array original", nomes, "\nSegunda Array copia", nomesCopia)
-console.log("Terceiro Array original", booleanos, "\nTerceiro Array copia", booleanosCopia)
+console.log("Primeira Array original", numeros, "Primeira Array copia", numerosCopia)
+console.log("Segunda Array original", nomes, "Segunda Array copia", nomesCopia)
+console.log("Terceiro Array original", misto, "Terceiro Array copia", mistoCopia)
+
+
+
+
