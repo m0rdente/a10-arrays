b61f899635937405526b221213599877bdd5d1b1
Priscila Mordente <priscilamordente@gmail.com>
2022-07-27 23:08:09 -0300

    Feito exercicio a10

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const numeros = [1,10,15,20,25]
+const nomes = ["Kagome", "Inuyasha","Kikyou"]
+const booleanos = [true, false]
+
+console.log (numeros)
+console.log (nomes)
+console.log (booleanos)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,12 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const numeros = [1,10,15,20,25]
+const nomes = ["Kagome", "Inuyasha","Kikyou"]
+const booleanos = [true, false]
+
+console.log("Quantidade de arrays na primeira", numeros.length)
+console.log("Quantidade de arrays na segunda", nomes.length)
+console.log("Quantidade de arrays na terceira", booleanos.length)
+console.log("Primeiro item do primeiro array", numeros[0])
+console.log("Segundo item do segundo array", nomes[1])
+console.log("Terceiro item do terceiro array", booleanos[2])
+console.log("Informação verdadeira primeiro Array", numeros.includes(1))
+console.log("Informação falsa terceiro Array", booleanos.includes())
